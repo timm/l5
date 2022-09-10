6856c0b6c314836cfe35115f6096269ab3ca2306
Tim Menzies <timm@ieee.org>
2022-09-09 22:37:51 -0400

    save

--- src/about.lua
+++ src/about.lua
@@ -18,3 +18,21 @@ OPTIONS:
  -s  --seed     random number seed                    = 10019
  -S  --sample   how many numbers to keep              = 512 ]]
 
+-- ## Coding Conventions
+-- 
+-- Every file should start with `require"lib"`.
+-- 
+-- Function argument conventions: 
+-- - 1. two blanks denote optionas, four blanls denote locals:
+-- - 2. prefix n,s,is,fun denotes number,string,bool,function; 
+-- - 3. suffix s means list of thing (so names is list of strings)
+-- - 4. c is a column index (usually)
+-- 
+-- Code starts with a help string, from which we extract settings to the `the`
+-- variable.
+-- 
+-- Code ends with `eg`s, containing demo code. The command line
+-- 
+--     lua eg.lua -e x
+-- 
+-- runs example `x`.
