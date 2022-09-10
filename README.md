13f476fac5f41f689f536328e8fd818f450eff04
Tim Menzies <timm@ieee.org>
2022-09-10 00:39:30 -0400

    save

---
+++ README.md
@@ -0,0 +1,9 @@
+<img align=right width=400 src="https://raw.githubusercontent.com/timm/shortr/master/docs/img/cup.png">
+
+# A little light learning library, in LUA
+
+For the last 20 years, my graduate students have explored explainable AI, multi-objective optimization, semi-supervised learning, configuration, planning, 
+etc, etc. Can I summarize all the work? Code it up in under 1000 lines of code? Refactor it into a library of learning tools that can be mixed and matched
+as required?
+
+Let's see...
--- src/about.lua
+++ src/about.lua
@@ -1,9 +1,9 @@
 return require("lib").settings[[   
 
-BOB : summarized csv file
+L5 : a little light learning library, in LUA
 (c) 2022 Tim Menzies <timm@ieee.org> BSD-2 license
 
-USAGE: lua bob.lua [OPTIONS]
+USAGE: lua l5.lua [OPTIONS]
 
 OPTIONS:
  -e  --eg       start-up example                      = nothing
@@ -18,21 +18,15 @@ OPTIONS:
  -s  --seed     random number seed                    = 10019
  -S  --sample   how many numbers to keep              = 512 ]]
 --    
--- ## Coding Conventions
+-- Coding Conventions
+-- - Every file should start with `require"lib"`.
+-- - Function argument conventions: 
+--   -  two blanks denote optionas, four blanls denote locals:
+--   -  prefix n,s,is,fun denotes number,string,bool,function; 
+--   -  suffix s means list of thing (so names is list of strings)
+--   -  c is a column index (usually)
+-- - Code starts with a help string, from which we extract settings to the `the`
+--  variable.
+-- - Code ends with `eg`s, containing demo code. To run some example `x` then::
 -- 
--- Every file should start with `require"lib"`.
--- 
--- Function argument conventions: 
--- - 1. two blanks denote optionas, four blanls denote locals:
--- - 2. prefix n,s,is,fun denotes number,string,bool,function; 
--- - 3. suffix s means list of thing (so names is list of strings)
--- - 4. c is a column index (usually)
--- 
--- Code starts with a help string, from which we extract settings to the `the`
--- variable.
--- 
--- Code ends with `eg`s, containing demo code. The command line
--- 
---     lua eg.lua -e x
--- 
--- runs example `x`.
+--     lua l5.lua -e x
