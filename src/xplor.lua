6cbb1a5c8e044b8c3143e2ec283b12fe63859e5c
Tim Menzies <timm@users.noreply.github.com>
2022-09-19 16:19:15 -0400

    save

--- src/xplor.lua
+++ src/xplor.lua
@@ -6,59 +6,70 @@ XPLOR: Bayesian active learning
 USAGE: lua XPLOR.lua [OPTIONS]
 
 OPTIONS:
- -f  --file  file with csv data                     =../data/auto93.csv
- -g  --go    start-up example                       =nothing
- -h  --help  show help                              =false
- -k  --k     Bayes hack: low attribute frequency    =1
- -m  --m     Bayes hack: low class frequency        =2
- -s  --seed  random number seed                     =10019]]
+ -f  --file  file with csv data                     = ../data/auto93.csv
+ -g  --go    start-up example                       = nothing
+ -h  --help  show help                              = false
+ -k  --k     Bayes hack: low attribute frequency    = 1
+ -m  --m     Bayes hack: low class frequency        = 2
+ -S  --Some  How many items to keep per row         = 256
+ -s  --seed  random number seed                     = 10019]]
 
-local coerce,count,csv,fmt,is,kap,keys,map
-local o,oo,push,same,sort,slots,sum
+local betters,coerce,count,csv,data1,DATA,fmt,is,kap,keys,locals,lt,map,norm
+local o,oo,push,same,some1,SOME,somes,sort,slots,sum
 
 -- ## Reading data
-_is={}
+local _is={}
 function _is.skip(s)  return s:find":$" end
-function _is.num(s)   return not _is.skip(s) and s:find"^[A-Z]" end
-function _is.goal(s)  return not _is.skip(s) and s:find"[!+-]$" end
-function _is.klass(s) return not _is.skip(s) and s:find"!$" end
-function _is.weight(s) 
-  if s:find"-$" then return -1 end
-  if s:find"+$" then return  1 end end
-
-function _ord(x,y) 
-  x= type(x)=="string" and -math.huge or x
-  y= type(y)=="string" and -math.huge or y
-  return x < y end
+function _is.num(s)   return  s:find"^[A-Z]" end
+function _is.goal(s)  return  s:find"[!+-]$" end
+function _is.klass(s) return  s:find"!$" end
+function _is.weight(s)  return s:find"-$" and -1 or 1 end
 
 -- ## NUM
-function NUM() --- constructor for summary of numbers
-  return {_has={},isSorted=true,n=0} end
+function SOME(n,s) --- constructor for summary of numbers
+  return {w=_is.weight(s or ""),at=n,name=s,_has={},isSorted=true,n=0} end
 
-function num1(num,n,    pos)
+function some1(some,n,    pos)
   if n~="?" then
-    self.n = self.n+1
-    if #self._has < the.Sample then pos=1+(#self._has)
-    elseif math.random()<the.Sample/self.n then pos=math.random(#self._has) end
-    if pos then self.isSorted=false
-                self._has[pos]= n end end end
+    some.n = some.n+1
+    if #some._has < the.Some then pos=1+(#some._has)
+    elseif math.random()<the.Some/some.n then pos=math.random(#some._has) end
+    if pos then some.isSorted=false
+                some._has[pos]= n end end end
 
-function nums(num)
-  if not num.isSorted then table.sort(num._has) end
-  num.isSorted=true
-  return num._has end
+function somes(some)
+  if not some.isSorted then table.sort(some._has) end
+  some.isSorted=true
+  return some._has end
+
+function norm(some,n,   t)
+  t=somes(some)
+  tmp =(t[#t] - t[1]) < 1E-9 and 0 or (n-t[1])/(t[#t] - t[1]) 
+  return tmp
+end
 
 -- ## DATA
 function DATA(t) 
-   return {names=t, rows={},
-           nums=kap(t,
-                    function(k,v) if is.num(v) then return NUM() end end)} end
+   return {names=t,rows={},
+           cols=kap(t,function(k,v) return SOME(k,t[k]) end)} end
 
 function data1(t,  data) --- return data, incremented with row `t`
   if not data then data = DATA(t) else
     push(data.rows,t)
-    kap(data.nums, function(k,num) num1(num,t[k]) end) end
+    map(data.cols, function(col) some1(col,t[col.at]) end) end
   return data end
+
+function betters(data,   order,cols,n)
+  cols = map(data.cols, function(col) if _is.goal(col.name) then return col end end)
+  local s1,s2,x,y=0,0
+  map(cols,oo)
+  function order(row1,row2)
+    for _,col in pairs(cols) do
+      x,y= norm(col,row1[col.at]), norm(col,row2[col.at])
+      s1 = s1 - 2.71828^(col.w * (x-y)/#cols)
+      s2 = s2 - 2.71828^(col.w * (y-x)/#cols)  end
+    return s1/#cols < s2/#cols end
+  return sort(data.rows, order) end
 
 -- ## Lib
 -- ### Lists
@@ -96,31 +107,23 @@ function oo(t)  --- Print a table `t` (non-recursive)
   print(o(t)) end 
 
 function o(t) ---  Generate a print string for `t` (non-recursive)
-  t= #t>0 and map(t,tostring) 
-          or  kap(slots(t),function(k,v) return fmt(":%s %s",k,t[k]) end) 
-  return "{".. table.concat(t,", ") .."}" end
+  if type(t) ~= "table" then return tostring(t) end
+  t = #t>0 and map(t,tostring)
+            or map(keys(t),function(v) return fmt(":%s %s",v,o(t[v])) end)
+  return "{".. table.concat(t," ") .."}" end
 
 -- ### Meta
-function same(x) --- return `x` 
-  return x end
-
-function count(t,fun)  --- Count how many Return sum of items in `t`, filtered via `fun` 
-  return sum(t, function(v) return 1 end) end
-
-function sum(t,fun)  --- Return sum of items in `t`, filtered via `fun` 
-  local n=0; for _,v in pairs(t) do n=n+fun(v) end; return n end
-
-function map(t,fun) --- Return `t`, filter through `fun(value)` (skip nils )
+function map(t,fun) --- Return `t`, filter through `fun(value)` (skip nils)
   local u={}; for _,v in pairs(t) do u[1+#u] = fun(v) end; return u end
 
-function kap(t1,fun) --- Return `t`, filtered via `fun(key,value)`, (skip nils) 
-  local t2={}; for k,v in pairs(t1) do t2[k] = fun(k,v) end; return t2 end
+function kap(t,fun) --- Return `t` and its size, filtered via `fun(key,value)`
+  local u={}; for k,v in pairs(t) do u[k]=fun(k,v) end; return u end
 
-function keys(t) --- Return keys of `t`, sorted (skip any with prefix  _`)
-  return sort(kap(t,function(key,_) 
+function keys(t) --- Return keys of `t`, sorted (skip any with prefix  `_`)
+  return sort(kap(t,function(key,_)  
                 if tostring(key):sub(1,1) ~= "_" then return key end end)) end
 
-function locals (    t,i) --- Return a list of local variables.
+function locals () --- Return a list of local variables.
   local i,t = 1,{}
   while true do
     local k,v = debug.getlocal(2, i)
@@ -130,7 +133,7 @@ function locals (    t,i) --- Return a list of local variables.
   return t end
 
 -- -----------------------------------------------------------------------------
-help:gsub("\n [-][%S]+[%s]+[-][-]([%S]+)[^\n]+=([%S]+)", function(k,v) 
+help:gsub("\n [-][%S]+[%s]+[-][-]([%S]+)[^\n]+= ([%S]+)", function(k,v) 
   for n,x in ipairs(arg) do
     if x=="-"..(k:sub(1,1)) or x=="--"..k then
       v = v=="false" and "true" or v=="true" and "false" or arg[n+1] end end
@@ -138,18 +141,17 @@ help:gsub("\n [-][%S]+[%s]+[-][-]([%S]+)[^\n]+=([%S]+)", function(k,v)
 
 if the.help then os.exit(print(help)) end
 
-d=nil
-csv(the.file, function(row) d=row1(d,row) end)
-oo(d)
+local go={}
+function go.csv(      data)
+  csv(the.file, function(row) data=data1(row,data)  end)
+  oo(data.cols[1]) end 
 
--- function betters(rows,nums) --- order two rows
---   n    = sum(cols,function(_) return 1 end)
---   function order(row1,row2)
---     for c,w in pairs(cols) do
---       x,y= row1[c], row2[c]
---       x,y= col:norm(x), col:norm(y)
---       s1 = s1 - math.exp(col.w * (x-y)/#ys)
---       s2 = s2 - math.exp(col.w * (y-x)/#ys) end
---   return s1/#ys < s2/#ys end
---
+function go.betters(      data,rows)
+  csv(the.file, function(row) data=data1(row,data)  end)
+  rows= betters(data) 
+  for i=1,#rows,30 do print(i,o(rows[i])) end end
+
+--go.csv()
+go.betters()
+for k,v in pairs(_ENV) do if not b4[k] then print("?",k,type(v)) end end
 local t=locals(); return t
