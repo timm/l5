b1e6f24b4c65f30e6bcaf41ff62e200f68d4d6a5
Tim Menzies <timm@ieee.org>
2022-09-22 05:59:20 -0400

    save

--- src/readme.lua
+++ src/readme.lua
@@ -1,48 +1,56 @@
-toc={}
+local tbl,obj,is={},{},{}
+local dump,lines,main,of,pretty,trim
+
+tbl={}
 obj={["funs"]="[fun]"}
 
+function hint(s1,type) --- if we know a type, add to arg. Else return arg
+    return type and s1..":`"..type .. "`" or s1 end
+
+function of(s)  --- try all the things
+  return obj[s] or is.str(s) or is.num(s) or is.tbl(s) or is.bool(s) or is.fun(s) end
+
+is={}
+function is.bool(s) if s:sub(1,2)=="is"     then return "bool" end end
+function is.fun(s)  if s:sub(#s - 2)=="fun" then return "fun" end end
+function is.num(s)  if s:sub(1,1)=="n"      then return "num" end end
+function is.str(s)  if s:sub(1,1)=="s"      then return "str"  end end
+function is.tbl(s)  if s=="t"               then return "tab" end end
+
+function pretty(s) --- clean up the signature
+    return s:gsub("[,]?    .*", ")")
+            :gsub(":new()",    "")
+            :gsub("([^, ()]+)", function(s1) 
+                                  s1=trim(s1); return hint(s1,of(s1)) end) end
+
+-- Low-level utils
 function lines(sFilename, fun,      src,s) --- call `fun` on csv rows.
   src  = io.input(sFilename)
   while true do
     s = io.read()
     if s then fun(s) else return io.close(src) end end end
 
-function dump()
-  if #toc>0 then
+function dump() --- if we have any tbl contents, print them then zap tbl
+  if #tbl>0 then
     print("\n| What | Notes |\n|:---|:---|")
-    for _,two in pairs(toc) do print("| "..two[1].." | ".. two[2] .." |") end 
+    for _,two in pairs(tbl) do print("| "..two[1].." | ".. two[2] .." |") end 
     print"\n" end
-  toc={} end 
+  tbl={} end 
 
-function trim(s) return s:match"^%s*(.-)%s*$" end 
+function trim(s) --- kill leading, trailing space
+  return s:match"^%s*(.-)%s*$" end 
 
-local is={}
-function is.str(s) if s:sub(1,1)=="s" then return "str"  end end
-function is.num(s) if s:sub(1,1)=="n" then return "num" end end
-function is.tbl1(s) if s=="t"          then return "tab" end end
-function is.bool(s) if s:sub(1,2)=="is" then return "bool" end end
-function is.fun(s) if s:sub(#s - 2)=="fun" then return "fun" end end
-
-function of(s) return obj[s] or is.str(s) or is.num(s) or is.tbl1(s) or is.bool(s) or is.fun(s) end
-
-function pretty(s)
-  local function annotate(s1,type) 
-    return type and s1..":`"..type .. "`" or s1 end
-  return s:gsub("[,]?    .*", ")")
-          :gsub(":new()",    "")
-          :gsub("([^, ()]+)", function(s1) 
-                               s1=trim(s1); return annotate(s1,of(s1)) end) end
-
-for i,file in ipairs(arg) do
-    print("\n#",file,"\n")
-    lines(file,function(line)
-      line:gsub("[A-Z][A-Z]+",      
-                function(x) obj[x:lower()]=x 
-                            obj[x:lower().."s"]="["..x.."]" end)
-      if line:find"^[-][-] " then
-        line:gsub("^[-][-] ([^\n]+)", 
-                  function(x) dump(); print(x:gsub("[-][-][-][-][-].*",""),"") end) 
-      else  
-        line:gsub("^function[%s]+([^-]+)[-][-][-] ([^\n]+)",
-                  function(what,notes) toc[1+#toc] = {pretty(what),notes} end) end end) 
-    dump() end 
+-- Main
+for _,file in ipairs(arg) do
+  print("\n#",file,"\n")
+  lines(file,function(line)
+    line:gsub("[A-Z][A-Z]+",      
+              function(x) obj[x:lower()]=x 
+                          obj[x:lower().."s"]="["..x.."]" end)
+    if line:find"^[-][-] " then
+      line:gsub("^[-][-] ([^\n]+)", 
+                function(x) dump(); print(x:gsub("[-][-][-][-][-].*",""),"") end) 
+    else  
+      line:gsub("^function[%s]+([^-]+)[-][-][-] ([^\n]+)",
+                function(lhs,rhs) tbl[1+#tbl] = {pretty(lhs),rhs} end) end end) 
+  dump() end 
