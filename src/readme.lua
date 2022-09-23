46bba79c193852fbefcc83289bd7ad39d8ccfa2b
Tim Menzies <timm@ieee.org>
2022-09-23 08:22:12 -0400

    save

--- src/readme.lua
+++ src/readme.lua
@@ -2,13 +2,7 @@ local tbl,obj,is={},{},{}
 local dump,lines,main,of,pretty,trim
 
 tbl={}
-obj={["funs"]="[fun]"}
-
-function hint(s1,type) --- if we know a type, add to arg. Else return arg
-    return type and s1..":`"..type .. "`" or s1 end
-
-function of(s)  --- try all the things
-  return obj[s] or is.str(s) or is.num(s) or is.tbl(s) or is.bool(s) or is.fun(s) end
+obj={}
 
 is={}
 function is.bool(s) if s:sub(1,2)=="is"     then return "bool" end end
@@ -16,6 +10,21 @@ function is.fun(s)  if s:sub(#s - 2)=="fun" then return "fun" end end
 function is.num(s)  if s:sub(1,1)=="n"      then return "num" end end
 function is.str(s)  if s:sub(1,1)=="s"      then return "str"  end end
 function is.tbl(s)  if s=="t"               then return "tab" end end
+
+function hint(s1,type) --- if we know a type, add to arg. Else return arg
+    return type and s1..":`"..type .. "`" or s1 end
+
+function of(s)  --- try all the things
+  return plural(s) or singular(s) end
+
+function plural(s,   s1,what) 
+  if s:sub(#s)=="s"  then  
+    s1   = s:sub(1,#s-1)
+    what = singular(s1)
+    return what and "["..what.."]"  or "tab" end end
+
+function singular(s) 
+  return obj[s] or is.str(s) or is.num(s) or is.tbl(s) or is.bool(s) or is.fun(s) end
 
 function pretty(s) --- clean up the signature
     return s:gsub("[,]?    .*", ")")
@@ -44,13 +53,14 @@ function trim(s) --- kill leading, trailing space
 for _,file in ipairs(arg) do
   print("\n#",file,"\n")
   lines(file,function(line)
-    line:gsub("[A-Z][A-Z]+",      
-              function(x) obj[x:lower()]=x 
-                          obj[x:lower().."s"]="["..x.."]" end)
+    line:gsub("[A-Z][A-Z]+", function(x) obj[x:lower()]=x end)
     if line:find"^[-][-] " then
       line:gsub("^[-][-] ([^\n]+)", 
                 function(x) dump(); print(x:gsub("[-][-][-][-][-].*",""),"") end) 
     else  
-      line:gsub("^function[%s]+([^-]+)[-][-][-] ([^\n]+)",
-                function(lhs,rhs) tbl[1+#tbl] = {pretty(lhs),rhs} end) end end) 
+      line:gsub("^function[%s]+[(]([^)]+)[)][%s]*[-][-][-] ([^\n]+)",
+                function(lhs,rhs) print(lhs); tbl[1+#tbl] = {pretty(lhs),rhs} end) end end) 
   dump() end 
+
+-- s:gsub("^function[%s]+[a-zA-Z0-9_]+[(](.*)[)](.*) [-][-][-] (.*)",function(x,y) print("::",x,y) end)
+-- s:gsub("^function[%s]+[a-zA-Z0-9_]+[(](.*)[)](.*)[%s]*[-][-][-]([.]*)$",function(x,y) print("::",x,y) end)
