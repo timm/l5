571e80f0e820322c4e2eb1747ac24d6fbf7c0105
Tim Menzies <timm@ieee.org>
2022-09-25 10:04:56 -0400

    save

--- src/tiny3.lua
+++ src/tiny3.lua
@@ -16,7 +16,7 @@ function keys(t)
   return sort(kap(t,want)) end
 
 -- strings
-fmt=string.format
+fmt = string.format
 function oo(t)      print(o(t)) end
 function o(t) 
   if type(t) ~= "table" then return tostring(t) end
@@ -49,18 +49,16 @@ function obj(s,    t,i,new)
 -------------------------------------------------------------------------------
 local SOME=obj"SOME"
 function SOME:new(max)
-  return {sorted=false,_has={},n=0,max=max or 512} end
+  return {sorted=false, _has={}, n=0, max=max or 512} end
 
 function SOME:add(x)
+  local function add(pos) self._has[pos]=x; self.sorted=false end
   if x ~= "?" then
-    local pos
-    self.n=self.n+1
-    if #self._has < self.max        then pos=1+#self._has 
-    elseif R()   < self.max/self.n then pos=R(#self._has) end
-    if pos then self._has[pos]=x; 
-                self.sorted=false end end end
+    self.n = self.n+1 
+    if #self._has < self.max        then add(1+#self._has)
+    elseif R()    < self.max/self.n then add(R(#self._has)) end end end 
 
-function SOME:sorted()
+function SOME:nums()
   if not self.sorted then table.sort(self._has) end
   self.sorted=true
   return self._has end
@@ -81,18 +79,39 @@ function COL:new(n,s)
 function COL:add(x) self.has:add(x); return self end
 
 function COL:norm(n)
-  local t=self:sorted(has)
+  local t=self.has:nums()
   return n=="?" and n or t[#t]-t[1]<1E-9 and 0 or (n-t[1])/(t[#t]-t[1]) end 
+
+function COL:discretize(x)
+  if not self.is.num then return x else
+    local t=self.has:nums()
+    local tmp = (t[#t] - t[1])/(the.bins - 1)
+    return t[#t]==t[1] and 1 or math.floor(x/tmp+.5)*tmp end end
+
+function COL:merge(xys, nMin) --- Can we combine any adjacent ranges?
+  if not self.is.num then return xys end
+  local function try2Merge(t,n,u)
+    while n <= #t do
+      local a,b = t[n], t[n+1]
+      local ab  = n < #t and simpler(a.y, b.y, nMin) -- <=== aas
+      u[1+#u]   = ab and XY(a.at, a.name, a.xlo, b.xhi, ab) or a
+      n         = ab and n+2 or n+1 end
+    return #t == #u and t or try2Merge(u,1,{}) 
+  end ---------------------
+  xys = try2Merge(xys,1,{})
+  for n = 2,#xys do xys[n].xlo = xys[n-1].xhi end   -- fill in any gaps
+  xys[1].xlo, xys[#xys].xhi = -math.huge, math.huge -- extend to +/- infinity
+  return xys end
 
 -------------------------------------------------------------------------------
 local DATA=obj"DATA"
 function DATA:new(names)
   self = {rows={}, cols={names=names, all={},x={},y={}}}
   for n,s in pairs(names) do
-    col = push(self.all, COL(n,s))
+    col = push(self.cols.all, COL(n,s))
     if not is.skip(s) then
-      if is.klass(s) then self.klass=col end
-      push(is.goal(s)  and self.y or self.x, col) end end
+      if is.klass(s) then self.cols.klass=col end
+      push(is.goal(s)  and self.cols.y or self.cols.x, col) end end
   return self end
 
 function DATA:dist(row1,row2)
@@ -116,11 +135,36 @@ function DATA:add(row)
     for _,col in pairs(cols) do 
      col:add(row[col.at]) end end end 
 
-function main(file)
-  local data
-  csv(file,function(row) oo(row);if data then data:add(row) else data=DATA(row) end end)
-  oo(data) end
+function load(file,    data)
+  csv(file, function(row) 
+              if data then data:add(row) else data=DATA(row) end end) 
+  oo(data.cols.all[1].has:nums()) end
+
+function DATA:xys(m,n)
+  local function xys(col,datas,      x,n,all,bin,xys)
+    n,all,xys = 0,{},{}
+    for y,data in pairs(datas) do
+      for _,row in pairs(data.rows) do
+        x = row[col.at]
+        if x ~= "?" then
+          n        = n+1
+          bin      = col:discretize(x) 
+          xys[bin] = xys[bin] or push(all, XY(col.at,col.name,x)) 
+          xys[bin]:add(x,y) end end end
+    return col:merge(sort(all,lt"xlo"), 
+                     n^the.Min) 
+  end -------------------------
+  local most,split     = -1,nil
+  local best,rest,rows = self:bestRest(m,n)
+  local B,R = #best, #rest
+  local out = {}
+  for _,col in pairs(self.cols.x) do
+    for i,xy in pairs(xys(col,{best = self:clone(best),
+                               rest = self:clone(rest)})) do
+      push(out, {xy=xy, score=xy.y:score("best",B,R)}) end end
+  return sort(out,gt"score") end
+
 
 -----
-main("../data/auto93.csv")
+load("../data/auto93.csv")
 
