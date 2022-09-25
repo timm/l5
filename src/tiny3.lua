030ba38f3d6cf605b721d9913b92b9516420db4c
Tim Menzies <timm@ieee.org>
2022-09-25 01:50:37 -0400

    save

--- src/tiny3.lua
+++ src/tiny3.lua
@@ -1,14 +1,22 @@
-fmt=sting.format
+local R,coerce,csv,fmt,kap,keys,map,o,obj,oo,push,sort
+-------------------------------------------------------------------------------
+-- maths
+R=math.random
 
+-- lists
 function last(t)    return t[#t] end
 function push(t, x) table.insert(t,x); return x end
-function sort(t, f) table.sort(t,f); return t end
-function map(t, f)  local u={};for _,v in pairs(t)do u[1+#u]=f(v)end;return u end
+function map(t, f)  local u={};for i,v in pairs(t)do u[1+#u]=f(v)end;return u end
 function kap(t, f)  local u={};for k,v in pairs(t)do u[k]=f(k,v)end; return u end
+
+-- sorting
+function sort(t, f) table.sort(t,f); return t end
 function keys(t) 
-  local function want(k,_) if tostring(k):sub(1,1) ~= "_" then return k end end
+  local function want(k,x) if tostring(k):sub(1,1) ~= "_" then return k end end
   return sort(kap(t,want)) end
 
+-- strings
+fmt=string.format
 function oo(t)      print(o(t)) end
 function o(t) 
   if type(t) ~= "table" then return tostring(t) end
@@ -16,6 +24,7 @@ function o(t)
   t = #t>0 and map(t,tostring) or map(keys(t),filter)
   return "{".. table.concat(t," ") .."}" end
 
+-- strings to things
 function coerce(s,    fun) --- Parse `the` config settings from `help`
   function fun(s1)
     if s1=="true"  then return true  end
@@ -31,6 +40,31 @@ function csv(sFilename, fun,      src,s,t) --- call `fun` on csv rows
     then t={}; for s1 in s:gmatch("([^,]+)") do t[1+#t]=coerce(s1) end; fun(t)
     else return io.close(src) end end end
 
+-- objects
+function obj(s,    t,i,new) 
+  local isa=setmetatable
+  function new(k,...) i=isa({},k); return isa(t.new(i,...) or i,k) end
+  t={__tostring = function(x) return s..o(x) end}
+  t.__index = t;return isa(t,{__call=new}) end
+-------------------------------------------------------------------------------
+local SOME=obj"SOME"
+function SOME:new(max)
+  return {sorted=false,_has={},n=0,max=max or 512} end
+
+function SOME:add(x)
+  if x ~= "?" then
+    local pos
+    self.n=self.n+1
+    if #self._has < self.max        then pos=1+#self._has 
+    elseif R()   < self.max/self.n then pos=R(#self._has) end
+    if pos then self._has[pos]=x; 
+                self.sorted=false end end end
+
+function SOME:sorted()
+  if not self.sorted then table.sort(self._has) end
+  self.sorted=true
+  return self._has end
+-------------------------------------------------------------------------------
 local is={}
 function is.skip(s)   return s:find":$"     end
 function is.num(s)    return s:find"^[A-Z]" end
@@ -38,48 +72,57 @@ function is.goal(s)   return s:find"[!+-]$" end
 function is.klass(s)  return s:find"!$"     end
 function is.weight(s) return s:find"-$" and -1 or 1 end
 
-function DATA() return {cols=nil,rows={}} end
+local COL=obj"COL"
+function COL:new(n,s)
+  n,s=n or 0, s or ""
+  return {has=SOME(), at=n, name=s,
+          is=kap(is,function(k,fun) print(s,k); return fun(s) end)} end 
 
-function COL(n,s)
-  return {has={}, at=n, name=s,
-          is=kap(is,function(_,fun) return fun(n) end)} end 
+function COL:add(x) self.has:add(x); return self end
 
-function COLS(names)
-  local cols={all={},x={},y={}}
+function COL:norm(n)
+  local t=self:sorted(has)
+  return n=="?" and n or t[#t]-t[1]<1E-9 and 0 or (n-t[1])/(t[#t]-t[1]) end 
+
+-------------------------------------------------------------------------------
+local DATA=obj"DATA"
+function DATA:new(names)
+  self = {rows={}, cols={names=names, all={},x={},y={}}}
   for n,s in pairs(names) do
-    col = push(cols.all, COL(n,s))
+    col = push(self.all, COL(n,s))
     if not is.skip(s) then
-      if is.klass(s) then cols.klass=col end
-      push(is.goal(s)  and cols.y or cols.x, cols) end end
-  return cols end
+      if is.klass(s) then self.klass=col end
+      push(is.goal(s)  and self.y or self.x, col) end end
+  return self end
 
-function norm(col,n,      lo,hi)
-  lo,hi=col.has[1], last(col.has) 
-  return hi - lo < 1E-9 and 0 or (x-lo)/(hi-lo) end 
-
-function dists(cols, row1,row2)
+function DATA:dist(row1,row2)
   local function dist(col,x,y)
     if x=="?" and y=="?" then return 1 end
     if not col.is.num then return x==y and 0 or 1 else
-      x,y = self:norm(x), self:norm(y)
+      x,y = col:norm(x), col:norm(y)
       if x=="?" then x = y<.5 and 1 or 0 end
       if y=="?" then y = x<.5 and 1 or 0 end
       return math.abs(x-y) end 
   end 
   local d,n=0,0
-  for _,col in pairs(cols.x) do
+  for i,col in pairs(self.cols.x) do
     n = n + 1
     d = d + dist(col,row1[col.at],row2[col.at])^2 end 
   return (d/n)^.5 end
 
-function read(row,data)
-  if not data.cols then data.cols=COLS(row) else
+function DATA:add(row,data)
+  if not data then data = DATA(row) else
     push(data.rows, row)
-    for n,x in pairs(row)do if x ~="?" then push(cols[n],x) end end end end
+    for _,cols in pairs{data.cols.x, data.cols.y} do
+      for _,col in pairs(cols) do 
+        col:add(row[col.at]) end end end 
+  return data end
 
 function main(file)
-  local rows,cols = {},{}
-  csv(file, read)
-  map(cols,function(col) table.sort(col.has) end)
-  
+  local data
+  csv(file,function(row) if data then data:add(row) else data=DATA(row) end end)
+  oo(data) end
+
+-----
+main("../data/auto93.csv")
 
